003c4e27bf695c3147faef44fb3679d9085a4eab
Matheus Pereira Gonsalves <matheus.gonsalvespereira@gmail.com>
2023-08-07 21:56:14 -0300

    Criando texto que digita sozinho

--- js/index.js
+++ js/index.js
@@ -1,11 +1,33 @@
-const navbar = document.querySelector(".navbar")
+//Variaveis
+const navbar = document.querySelector(".navbar");
+const elTexto = document.querySelector(".typing");
+const text = 'Desenvolvedor Front-End'
+const interval = 60;
 
-window.addEventListener("scroll", () => {
+
+
+
+window.addEventListener("scroll", () => {//Animacao da navbar quando o scroll for acionado.
     if (window.scrollY > 40) {
-        navbar.classList.add("bg-dark")
+        navbar.classList.add("bg-dark");
     }
     else {
-        navbar.classList.remove("bg-dark")
+        navbar.classList.remove("bg-dark");
     }
 })
 
+function textoDigitando(el, texto, intervalo) {
+    const char = text.split("").reverse();
+
+    const typer = setInterval(()=>{
+        if(!char.length)
+        {
+            return clearInterval(typer);
+        }
+        const next = char.pop();
+
+        elTexto.innerHTML += next
+    }, interval);
+}
+
+textoDigitando(elTexto, text, interval)
