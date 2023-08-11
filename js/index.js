7e1ac39e8237944f268ddd5f221ff91f1d9779f4
Matheus Pereira Gonsalves <matheus.gonsalvespereira@gmail.com>
2023-08-10 23:51:43 -0300

    Melhorando estilizações e animações do menu lateral. Responsividade para algumas telas incompleta.

--- js/index.js
+++ js/index.js
@@ -20,22 +20,69 @@ document.addEventListener("DOMContentLoaded", digitando);
 const navbar = document.querySelector(".navbar");
 const elTexto = document.querySelector(".typing");
 const elMenuLateral = document.querySelector(".menu-lateral");
-const elDireita = document.querySelector(".direita");
+const elNavbar = document.querySelector(".navbar");
+const elHouse = document.querySelector(".house");
+const elAbout = document.querySelector(".about-me");
+const elProjetos = document.querySelector(".project");
+const elContato = document.querySelector(".contact");
 
-//Animacao da navbar quando o scroll for acionado.
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 40) {
-        navbar.classList.add("bg-dark");
+//evento de animacao do menu lateral
+elMenuLateral.addEventListener("click", showMenu);
+
+//Funcao para animacao do menu lateral
+function showMenu()
+{
+    if(elNavbar.classList.contains("open")){
+        elNavbar.classList.remove("open");
+        elHouse.innerHTML = 
+        `
+        <a class="nav-link house" href="#home">
+            <li><i class="bi bi-house-door"></i></li>
+        </a>
+        `
+        elAbout.innerHTML = 
+        `
+        <a class="nav-link about-me" href="#sobre">
+            <li><i class="bi bi-file-person"></i></li>
+        </a>
+        `
+        elProjetos.innerHTML = 
+        `
+        <a class="nav-link project" href="#projetos">
+            <li><i class="bi bi-kanban-fill"></i></li>
+        </a>
+        `
+        elContato.innerHTML = 
+        `
+        <a class="nav-link contact" href="#contato">
+            <li><i class="bi bi-person-circle"></i></li>
+        </a>
+        `
+    }else {
+        elNavbar.classList.add("open");
+        elHouse.innerHTML = 
+        `
+        <a class="nav-link house" href="#home">
+            <li><i class="bi bi-house-door"></i><span class="spanNav">Home</span></li>
+        </a>
+        `
+        elAbout.innerHTML = 
+        `
+        <a class="nav-link about-me" href="#sobre">
+            <li><i class="bi bi-file-person"><span class="spanNav">Sobre Mim</span></i></li>
+        </a>
+        `
+        elProjetos.innerHTML = 
+        `
+        <a class="nav-link project" href="#projetos">
+            <li><i class="bi bi-kanban-fill"><span class="spanNav">Projetos</span></i></li>
+        </a>
+        `
+        elContato.innerHTML = 
+        `
+        <a class="nav-link contact" href="#contato">
+            <li><i class="bi bi-person-circle"><span class="spanNav">Contato</span></i></li>
+        </a>
+        `
     }
-    else {
-        navbar.classList.remove("bg-dark");
-    }
-})
-
-//Animacao do menu lateral
-elMenuLateral.addEventListener("click", ()=>{
-    
-})
-
-
-
+}
